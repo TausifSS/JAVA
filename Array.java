edc3621550907249e2253328ef0c8ef649ba90c7
Tausif Shaikh <new-tausifss015@gmail.com>
2024-08-16 09:16:53 +0530

    Changing name

--- Array.java
+++ Array.java
@@ -1,6 +1,6 @@
 package JAVA;
 import java.util.*;
-public class arr {
+public class Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // level 1;
