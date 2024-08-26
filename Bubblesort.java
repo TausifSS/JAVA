d44ad70befb8ebe9a6dfc94a19807b9a853b0dc0
TausifSS <tausifss015@gmail.com>
2024-08-26 20:27:19 +0530

    Update Bubblesort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -1,1 +1,21 @@
-
+public class Sort {
+    public static void printArr(int arr[]){
+    for (int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+    }
+    }
+    public static void main(String[] args) {
+        int arr[] = {7, 8, 5, 4, 2, 3, 1};
+        for (int i=0;i<arr.length;i++){
+            for (int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>=arr[j+1]){
+                    //swap
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        printArr(arr);
+    }
+}
